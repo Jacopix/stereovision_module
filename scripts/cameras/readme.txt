1a569adef26f701b43943f82f1651e309f135259
Jacopo <none>
2024-11-18 23:57:47 +0100

    Added all cameras, added lower depth limit

---
+++ scripts/cameras/readme.txt
@@ -0,0 +1,23 @@
+Information regarding compatible_cameras.json
+
+
+Source: 
+    https://developer.nvidia.com/embedded/jetson-partner-supported-cameras
+
+
+File structure
+    all_cameras
+        All cameras compatible with Jetson Nano and MIPI interface
+
+    available_cameras (subgroup of all_cameras)
+        All cameras for which a vendor was found
+
+    lowlight_cameras (subgroup of available_cameras)
+        All cameras with "low-light" feature
+
+Unit of measurement
+    price:          $ or €
+    pixel size:     micrometers [um]
+    sensor size:    pixels
+    hfov:           degrees
+    
--- scripts/depth_quality.py
+++ scripts/depth_quality.py
@@ -13,26 +13,26 @@ MAX_Y = 1500      # [mm]
 # Error expectations: 
 #   < 0.8% @ 2m
 #   < 4.0% @ 12m    
-# init_p_width    = 2     # um
-# init_p_heigth   = 2     # um
-# init_s_width    = 2688  # pixels
-# init_s_heigth   = 1520  # pixels
-# init_hfov       = 110    # degrees
-# init_baseline   = 120   # mm
-# init_disparity  = 1     # pixels
+init_p_width    = 2     # um
+init_p_heigth   = 2     # um
+init_s_width    = 2688  # pixels
+init_s_heigth   = 1520  # pixels
+init_hfov       = 110    # degrees
+init_baseline   = 120   # mm
+init_disparity  = 1     # pixels
 
 # ZED 2i stereocamera (model 2)
 #
 # Error expectations: 
 #   < 0.4% @ 2m
 #   < 7.0% @ 20m    
-init_p_width    = 2     # um
-init_p_heigth   = 2     # um
-init_s_width    = 2688  # pixels
-init_s_heigth   = 1520  # pixels
-init_hfov       = 72    # degrees
-init_baseline   = 120   # mm
-init_disparity  = 1     # pixels
+# init_p_width    = 2     # um
+# init_p_heigth   = 2     # um
+# init_s_width    = 2688  # pixels
+# init_s_heigth   = 1520  # pixels
+# init_hfov       = 72    # degrees
+# init_baseline   = 120   # mm
+# init_disparity  = 1     # pixels
 
 # Gemini 335/336
 #
@@ -68,6 +68,10 @@ def get_focal_length_mm(s_width, s_heigth, p_width, p_heigth, hfov):
 def d_z(z, disparity_step, baseline, focal_length):
     return (pow(z,2) * disparity_step) / (baseline * focal_length + z * disparity_step)
 
+def get_min_depth(hfov, baseline):
+    return round( baseline / ( np.tan( (hfov * np.pi) / (180 * 2) ) ) ) # min_depth is where the two fov overlaps at least 50% 
+    #return round( baseline / ( 2 * np.tan( (hfov * np.pi) / (180 * 2) ) ) ) # min_depth is where the two fov intersept each other for the first time
+
 
 # Set up the graph
 z = np.linspace(0, MAX_X, 100)
@@ -78,13 +82,17 @@ plt.ylim(0,MAX_Y)
 ax.set_xlabel('Depth [mm]')
 ax.set_ylabel('Error [mm]')
 
+# Set up minimum detph line
+min_depth = ax.axvline(get_min_depth(init_hfov, init_baseline), color='r', lw=2)
 
 # Set up space for other widgets 
 fig.subplots_adjust(bottom=0.4)
 gs_boxes = gridspec.GridSpec(8,1, wspace=2)
 gs_sliders = gridspec.GridSpec(1,1, wspace=2)
-gs_boxes.update(left=0.2, right=0.5, bottom=0.01, top=0.3, hspace=1,)
-gs_sliders.update(left=0.7, right=0.9, bottom=0.01, top=0.1, hspace=1,)
+gs_others = gridspec.GridSpec(1,1, wspace=2)
+gs_boxes.update(left=0.2, right=0.5, bottom=0.01, top=0.3, hspace=1)
+gs_sliders.update(left=0.7, right=0.9, bottom=0.01, top=0.1, hspace=1)
+gs_others.update(left=0.7, right=0.9, bottom=0.28, top=0.3, hspace=1)
 
 # create textboxes
 p_width         = TextBox(fig.add_subplot(gs_boxes[0,0]), 'pixel width [um]', hovercolor='0.975', label_pad=0.1)
@@ -95,6 +103,7 @@ hfov            = TextBox(fig.add_subplot(gs_boxes[4,0]), 'hfov [deg]', hovercol
 disparity       = TextBox(fig.add_subplot(gs_boxes[5,0]), 'disparity step [px]', hovercolor='0.975', label_pad=0.1)
 focal_length_mm = TextBox(fig.add_subplot(gs_boxes[6,0]), 'focal length [mm]', hovercolor='0.975', label_pad=0.1)
 focal_length_px = TextBox(fig.add_subplot(gs_boxes[7,0]), 'focal length [px]', hovercolor='0.975', label_pad=0.1)
+min_depth_box   = TextBox(fig.add_subplot(gs_others[0,0]), 'minimum depth [mm]', hovercolor='0.975', label_pad=0.1)
 
 textboxes = [p_width, p_heigth, s_width, s_heigth, hfov, disparity, focal_length_mm, focal_length_px]
 
@@ -119,6 +128,7 @@ def update(val):
     val_s_heigth   = float(s_heigth.text)
     val_hfov       = float(hfov.text)
     val_disparity  = float(disparity.text)
+    val_baseline   = baseline.val
 
     # Calculate and set focal length [px]
     val_fl_px = get_focal_length_px(val_s_width, val_s_heigth, val_hfov)
@@ -128,8 +138,13 @@ def update(val):
     val_fl_mm = get_focal_length_mm(val_s_width, val_s_heigth, val_p_width, val_p_heigth, val_hfov)
     focal_length_mm.set_val(str(val_fl_mm))
 
+    # Change min depth line
+    new_min_depth = get_min_depth(val_hfov, val_baseline)
+    min_depth.set_xdata([new_min_depth, new_min_depth])
+    min_depth_box.set_val(new_min_depth)
+
     # Draw graph
-    line.set_ydata(d_z(z, val_disparity, baseline.val, val_fl_px))
+    line.set_ydata(d_z(z, val_disparity, val_baseline, val_fl_px))
     fig.canvas.draw_idle()
 
 # Register the update function with each slider or text box
